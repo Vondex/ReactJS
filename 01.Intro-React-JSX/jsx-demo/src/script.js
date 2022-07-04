80a23d0b826bb89356201ffc0ad2035319a1dc72
Veronika Naydenova <veronica.naydenova@gmail.com>
2022-07-04 18:58:31 +0300

    jsx added

--- 01.Intro-React-JSX/jsx-demo/src/script.js
+++ 01.Intro-React-JSX/jsx-demo/src/script.js
@@ -1,1 +1,11 @@
 const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+const reactElement = <header className="site-header">
+
+    <h1>Hello from JSX!</h1>
+    <h2>Something crazy xp</h2>
+</header>
+
+
+root.render(reactElement);
