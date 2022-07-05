3a684f709ddb5222f631fa5573aee5668369e8fa
Veronika Naydenova <veronica.naydenova@gmail.com>
2022-07-05 14:54:26 +0300

    added demos

---
+++ 01.Intro-React-JSX/create-react-app-demo/src/App.js
@@ -0,0 +1,16 @@
+import Footer from './Footer';
+import { Header } from './Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Header text="Hello" />
+     <Footer />
+     
+    </div>
+  );
+}
+
+export default App;
